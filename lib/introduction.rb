1cbc112557d8eeeb4457c66fda002d322d9a82b1
Phoenix R <phoenixrabang@gmail.com>
2020-01-14 13:34:55 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,8 +11,8 @@ def introduction_with_language(name,language)
   "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end
 
-introduction
-introduction_with_language
+introduction(name)
+introduction_with_language(name,language)
 
 
 
